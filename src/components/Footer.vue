41feec32594f17363005151588232b080bb4c8f5
Izuka Ikedionwu <117411866+izukaike@users.noreply.github.com>
2025-01-11 08:33:32 -0600

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer">
       <div class="content has-text-centered">
-        <p>&copy; 2024 Full Name. All rights reserved.</p>
+        <p>&copy; 2024 Izuka Ikedionwu. </p>
       </div>
     </footer>
   </template>
